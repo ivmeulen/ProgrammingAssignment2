a413e3e6469a21c8eaa639bf0d992f7f7422d372
ivmeulen <70780151+ivmeulen@users.noreply.github.com>
2020-10-26 21:16:04 -0600

    Update cachematrix.R
    I cleaned things up, added comments, and made sure everything worked properly. I want the bonus points for clarity and cleanliness.

--- cachematrix.R
+++ cachematrix.R
@@ -5,15 +5,16 @@
 ## Important to note, this function does NOT calculate the inversion that it stores. It assumes that
 ## whatever matrix you feed it (as the inversion) has been correctly calculated elsewhere.
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()){
+  # Make an empty object for the inverse matrix to occupy
   invert <- NULL
   set <- function(y){
     x <<- y
     invert <<- NULL
   }
   get_data <- function() x
-  set_inverse <- function(i) inversion <<- i
-  get_inverse<- function() inversion
+  set_inverse <- function(i) invert <<- i
+  get_inverse<- function() invert
   list(set = set, get_data = get_data,
        set_inverse = set_inverse,
        get_inverse = get_inverse)
@@ -24,14 +25,36 @@ makeCacheMatrix <- function(x = matrix()) {
 ## enable and empower it.
 
 cacheSolve <- function(x, ...) {
-  i <- x$get_inverse()
-  if(!is.null( i)){
+  
+  # First check if the existing "matrix" has a NULL inversion
+  check_inv <- x$get_inverse()
+  if(!is.null(check_inv)){
+    
+    ## If inverse is not NULL, then pull it from the cached value
     message("getting cached data")
-    return(i)
-  }
-  data <- x$get
-  inverse <- solve(data, ...)
-  x$setinverse(inverse)
-  inverse
-        ## Return a matrix that is the inverse of 'x'
-}
+    inv <- x$get_inverse()
+    
+    ## Define a variable, then spit out the variable from the environment
+    inv
+    
+  # Set the else{} to match the if{}
+ }else{
+   
+   # inform the user that this is a fresh operation
+   message("calculating new inversion")
+   
+   # Get the data with the function previously described in the list
+   invData <- x$get_data()
+   
+   # Calculate the inversion from that data
+   inversion <- solve(invData, ...)
+   
+   # Set the inversion
+   x$set_inverse(inversion)
+   
+   # Assign the inversion to a variable
+   i <- x$get_inverse()
+   
+   # Report the inversion (a matrix that is the inverse of 'x')
+   i
+} }
